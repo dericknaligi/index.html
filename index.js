82297a3e5fa57e5124fba93f81d11a5716eb14e5
derick naligi <chahasid@gmail.com>
2024-10-10 14:43:09 +0300

    queenspharma

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("HELLO");
+console.log("this is my web page")
+document.getElementById("myH1").textContent= "QueensPharma ";
+document.getElementById("myP").textContent = "Login"
+let age = 25;
+let price = 455.66;
+let gpa = 23;
+console.log("Your are", age  )
